9179ee41f62484d831d251f71fd6dbda96127d97
Lucas Simpson <lucassimpson0213@gmail.com>
2026-03-19 20:40:27 -0400

    added ingestor, api and worker features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#[cfg(feature = "ingestor")]
+pub mod ingestor;
+
+#[cfg(feature = "api")]
+pub mod api;
+
+#[cfg(feature = "worker")]
+pub mod worker;
+
 fn main() {
     println!("Hello, world!");
 }
